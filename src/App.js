e0de1a860c4910ae6397e9e3e7d869ed6d9edc77
Aminah Burch <hello@aminah.io>
2020-04-05 15:01:09 -0500

    Initial commit of react app

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+	<p>Well well well, here we are again. You always coming back to the code. When will it stick? When will you FINALLY get your coding job? Huh????</p> 
         <a
           className="App-link"
           href="https://reactjs.org"
