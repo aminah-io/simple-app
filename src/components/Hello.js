1e24b7ea467af548015188fe6a1b710f90b31a06
Aminah Burch <hello@aminah.io>
2020-04-06 22:01:50 -0500

    Component state, props, lifecycle

---
+++ src/components/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Media } from 'reactstrap';
+
+class Hello extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <h2>Don't Let Go, Okay?</h2>
+      </div>
+    );
+  }
+}
+
+export default Hello;
